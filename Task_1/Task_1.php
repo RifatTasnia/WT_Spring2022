fc520d8ed6af017406a685eda9962838764b8c82
RifatTasnia <98705956+RifatTasnia@users.noreply.github.com>
2022-02-02 13:22:45 +0600

    Add files via upload

---
+++ Task_1/Task_1.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<body>
+<h1>Registration Form</h1>
+<form action="/action_page.php">
+
+First Name:
+<input type="text" name="First Name"><br>
+
+Last Name:
+<input type="text" name="Last Name"> <br>
+
+Age:
+<input type="number"><br>
+
+Designation:
+<input type="radio" name="Designation">Junior Programmer
+<input type="radio" name="Designation">Senior Programmer
+<input type="radio" name="Designation">Project Lead<br>
+
+Preferred language 
+<input type="checkbox">JAVA
+<input type="checkbox">PHP
+<input type="checkbox">C++<br>
+
+E-mail: 
+<input type="mail" name="E-mail"><br>
+
+Password: 
+<input type="password" name="Password"><br>
+
+<input type="submit" value="Submit">
+<input type="reset" value="Reset">
+
+
+
+</form>
+</body>
+</html>
